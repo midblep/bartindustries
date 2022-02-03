fd9c4a82922f6f8e18ccf208d379a6a9c9996ccb
midblep <mrrmiddynight@gmail.com>
2022-02-03 18:01:18 +0100

    update sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: "#svelte",
 	},
 };
 
