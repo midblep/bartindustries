549f71b3b0b2ed3fdc14da3fce1480e3b5093c45
midblep <mrrmiddynight@gmail.com>
2022-05-06 21:43:59 +0200

    fix

--- src/lib/home/Card.svelte
+++ src/lib/home/Card.svelte
@@ -4,7 +4,7 @@
 	export let buttonHref = null;
 	export let disabled = false;
 
-	let classes = "z-10 flex flex-col group relative overflow-hidden";
+	let classes = "z-10 flex flex-col group relative";
 
 	if(disabled) {
 		classes += " opacity-50";
@@ -13,6 +13,10 @@
 
 <div class={classes}>
 	<span class="h-4 group-hover:-translate-y-1 bg-blue-600 rounded-t-xl duration-300"></span>
+
+	{#if disabled}
+		<span class="mt-10 flex justify-center text-center items-center text-black dark:text-white font-bold text-2xl group-hover:-translate-y-1 duration-300 select-none">UNFINISHED PROJECT</span>
+	{/if}
 
 	<div class="flex-grow flex flex-col gap-2 w-full relative p-10 group-hover:-translate-y-1 duration-300 bg-gradient-to-br from-gray-200 to-gray-300 dark:from-gray-800 dark:to-gray-700">
 		<slot />
@@ -28,9 +32,5 @@
 		</a>
 	{:else}
 		<span class="h-4 bg-opacity-50 group-hover:-translate-y-1 bg-white dark:bg-gray-900 rounded-b-xl duration-300"></span>
-	{/if}
-
-	{#if disabled}
-		<span class="absolute top-0 bottom-0 flex justify-center text-center items-center text-black dark:text-white -rotate-45 font-bold text-6xl group-hover:-translate-y-1 duration-300 select-none">UNFINISHED PROJECT</span>
 	{/if}
 </div>
