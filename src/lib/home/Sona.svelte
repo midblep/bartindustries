8bdd6bf6db3bad18e34522c42d8f6ac567e50535
Midblep <mrrmiddynight@gmail.com>
2022-08-30 14:04:39 +0200

    mobile styling

--- src/lib/home/Sona.svelte
+++ src/lib/home/Sona.svelte
@@ -1,16 +1,16 @@
-<main class="flex flex-col gap-20 h-screen">
-	<section class="relative w-full h-full">
+<main class="flex flex-col gap-20 min-h-screen">
+	<section class="w-full h-full">
 		<!-- <div class="absolute inset-0 blur-xl overflow-hidden">
 			<img src="/furry/Background.png" class="w-full scale-110" alt="background" />
 		</div> -->
-		<article class="absolute inset-0 flex container mx-auto py-20 flex-col col-span-2 gap-10 items-center italic">
-			<div class="flex items-center gap-10">
-				<img src="/furry/MidFingers.jpg" class="rounded-full h-40 shadow-xl aspect-square" alt='background' />
+		<article class="flex container mx-auto py-20 flex-col col-span-2 gap-10 items-center italic">
+			<div class="flex items-center gap-5 lg:gap-10">
+				<img src="/furry/MidFingers.jpg" class="rounded-full h-32 lg:h-40 shadow-xl aspect-square" alt='background' />
 				<span>
-					<img src="/homepage/sona/midtitle.png" class="w-40" alt="fullbody of mid" />
+					<img src="/homepage/sona/midtitle.png" class="w-32 lg:w-40" alt="fullbody of mid" />
 				</span>
 			</div>
-			<h4 class="w-1/2 text-center font-normal">
+			<h4 class="w-10/12 lg:w-1/2 text-center font-normal">
 				Mid <sup>(aka Midnight)</sup> is my primary fursona. He is a fictional character that I use and represent myself with online.
 				If you are wondering what a furry or fursona is, go check <a href="https://www.whatisfurry.org/" target="blank" class="text-blue-600 dark:text-blue-300 hover:underline">whatisfurry.org</a>.
 				<br/><br/>
@@ -21,7 +21,7 @@
 			</h4>
 
 			<div class="flex justify-center w-full flex-col gap-5">
-				<div class="w-1/2 mx-auto grid grid-cols-4 gap-5">
+				<div class="w-10/12 lg:w-1/2 mx-auto grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-5">
 					<img src="/homepage/sona/midblep_2-2_sivkyne.png" class="rounded-xl aspect-square object-contain" alt="sona symbol" />
 					<img src="/homepage/sona/midblep_3.png" class="rounded-xl aspect-square" alt="sona symbol" />
 					<img src="/homepage/sona/midblep_7 (1).png" class="rounded-xl aspect-square" alt="sona symbol" />
